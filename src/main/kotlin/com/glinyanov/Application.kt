2edd0f552a564ce5a671b43c2083957e2d6644b4
Glinyanov Sergey <sglinyanov@gmail.com>
2023-05-20 22:46:45 +0300

    add jar

--- src/main/kotlin/com/glinyanov/Application.kt
+++ src/main/kotlin/com/glinyanov/Application.kt
@@ -22,7 +22,7 @@ fun Application.module() {
         issuer = environment.config.property("jwt.issuer").getString(),
         audience = environment.config.property("jwt.audience").getString(),
         exiresIn = 365L * 1000L * 60L * 24L,
-        secret = System.getenv("JWT_SECRET")
+        secret = "JWT_SECRET"
     )
     val hashingService = SHA256HashingService()
     configureSerialization()
